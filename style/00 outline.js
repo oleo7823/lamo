18c77d3466f0194613cb7f3970043521822a3848
Leo-QL <leojustme723@gmail.com>
2024-08-07 17:54:53 +0800

    outline of morphosyntax_en

--- style/00 outline.js
+++ style/00 outline.js
@@ -1,23 +1,43 @@
+window.addEventListener('scroll', function() {
+    var nav = document.querySelector('nav');
+    var article = document.querySelector('article');
+    var outline = document.querySelector('.outline');
+    var navRect = nav.getBoundingClientRect();
+    var articleRect = article.getBoundingClientRect();
+    var outlineRect = outline.getBoundingClientRect();
+    
+    // Determine if the article is below the sticky nav
+    var navBottom = navRect.bottom;
+    var articleTop = articleRect.top;
+    var outlineHeight = outlineRect.height;
+
+    if (articleTop < navBottom) {
+        // Article is below the nav, fix the outline below the nav
+        outline.style.position = 'fixed';
+        outline.style.top = (navBottom) + 'px';
+        outline.style.left = '0';
+    } else {
+        // Article is above the nav, keep the outline in its place
+        outline.style.position = 'absolute';
+        outline.style.top = '0'; // Reset to original position
+        outline.style.left = '0';
+    }
+});
 function initTree(t) {
     var tree = document.getElementById(t); // 生成树
-    var lis = tree.getElementsByTagName("ul"); // 所有子菜单
+    var ps = tree.getElementsByTagName("p"); // 所有主菜单
 
-    for (var i = 0; i < lis.length; i++) {
-        lis[i].style.display = "none"; // 设置样式为隐藏形式，不显示
-        var ps = lis[i].getElementsByTagName("p"); // 子菜单的标题
-        for (var j = 0; j < ps.length; j++) {
-            ps[j].onclick = function() {
-                openTag(this, this.nextElementSibling);
+    for (var i = 0; i < ps.length; i++) {
+        ps[i].onclick = function() {
+            var nextUl = this.nextElementSibling;
+            if (nextUl && nextUl.tagName === "UL") {
+                if (nextUl.style.display === "block") {
+                    nextUl.style.display = "none";
+                } else {
+                    nextUl.style.display = "block";
+                }
             }
         }
-    }
-}
-
-function openTag(p, ul) {
-    if (ul && ul.style.display === "block") { // 点击一些展开，再点击一下折叠
-        ul.style.display = "none";
-    } else if (ul) {
-        ul.style.display = "block";
     }
 }
 
